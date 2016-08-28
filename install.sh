f7b5a6c510b80919c39c9ddf7950fc161dfddfae
qkniep <hello@quentinkniep.me>
2016-08-28 16:28:29 +0200

    add example ssh config and clean up install script

--- install.sh
+++ install.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
-# This script installs everything needed for my Linux configuration
+# This script sets up everything needed for my Linux configuration.
 
-########## Variables
+
+# --------------------------------------------------------------
+# Variables
+# --------------------------------------------------------------
 
 dir=~/dotfiles         # dotfiles directory
-olddir=~/dotfiles_old  # old dotfiles backup directory
-# list of files/folders to symlink in
-files="bashrc bash_aliases vimrc"
-homedir
+olddir=~/dotfiles_old  # backup directory
+files=".bashrc .bash_aliases .vimrc .ssh/config"
 
-##########
+# --------------------------------------------------------------
+# Dotfiles
+# --------------------------------------------------------------
 
-# create dotfiles_old in home
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
 cd $dir
-echo "done"
-
-# move old dotfiles to dotfiles_old + create symlinks
 for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
-	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+	newfile=${file//.}
+	newfile=${newfile//\//_}
+	mv ~/$file $olddir/$newfile  # move old dotfiles
+	ln -s $dir/$newfile ~/$file  # create symlinks
 done
 
-##########
 
-echo "Installing vim and git"
-apt install vim
-apt install git
-echo "done"
+# --------------------------------------------------------------
+# Software
+# --------------------------------------------------------------
 
-##########
+apt install vim        # text editor
+apt install git        # version control
+apt install unp        # archive unpacker
+apt install mpsyt      # youtube CLI audio-player
+apt install weechat    # IRC client
+apt install cmatrix    # matrix animation
+apt install ttu-clock  # terminal clock
 
-echo "Installing Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "done"
 
-##########
+# --------------------------------------------------------------
+# Github
+# --------------------------------------------------------------
 
-# install base16 shell colors
-echo "Installing base16 shell colors from github.com/chriskempson/base16-shell"
-git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-echo "done"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim     # vim plugin manager
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell  # base16 shell colors
