d8792f1f5acea356c59d2503d2173a4e8c1e86a1
Quentin Kniep <hello@quentinkniep.com>
2026-06-15 01:17:40 +0200

    fix: hopefully fixed Claude status in tmux tabline now (#24)

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -968,7 +968,7 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.ERROR] = '✘',
       [vim.diagnostic.severity.WARN] = '',
       [vim.diagnostic.severity.HINT] = '⚑',
-      [vim.diagnostic.severity.INFO] = '»',
+      [vim.diagnostic.severity.INFO] = '', -- alternative: »
     },
   },
 })
